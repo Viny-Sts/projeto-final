c526d9d95a3da2662d6122aa5590d1a38175321d
rodrigo <limaosuco@gmail.com>
2023-04-11 15:19:01 -0300

    Project updated

--- src/main/java/br/edu/ifg/luziania/Login.java
+++ src/main/java/br/edu/ifg/luziania/Login.java
@@ -28,13 +28,23 @@ public class Login {
     @Path("/autenticar")
     public Response authenticate(Auth auth){
         AuthReturn authReturn = new AuthReturn();
-        if (auth.getEmail().equals("daniel@ifg.edu.br") &&
-            auth.getPassword().equals("123")) {
+
+        if (auth.getEmail().equals("rodrigo@gmail.com") && auth.getPassword().equals("projeto") ||
+                auth.getEmail().equals("vinicius@gmail.com") && auth.getPassword().equals("123")) {
             authReturn.setMessage("Usuário autenticado!");
+
             return Response.ok(authReturn, MediaType.APPLICATION_JSON).build();
         }
+
+        else if (auth.getEmail().equals("admin@staff.com") && auth.getPassword().equals("123")) {
+            authReturn.setMessage("Administrador autenticado!");
+
+            return Response.ok(authReturn, MediaType.APPLICATION_JSON).build();
+        }
+
         else {
             authReturn.setMessage("Usuário não autenticado!");
+
             return Response.ok(authReturn, MediaType.APPLICATION_JSON).build();
         }
     }
