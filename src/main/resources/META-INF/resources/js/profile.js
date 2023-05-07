f16363aaefa809ac8bb50747659c8d093f326de7
rodrigo <limaosuco@gmail.com>
2023-05-07 12:06:58 -0300

    StormTrack 105 (Rodrigo's branch) - Added profile.js for dynamic profile table control. Now all profiles will be added in a new entry

--- src/main/resources/META-INF/resources/js/profile.js
+++ src/main/resources/META-INF/resources/js/profile.js
@@ -1,22 +1,34 @@
-profileIndex = 0;
+let profileIndex = 0;
 
 function addProfile() {
+    let permissions = [document.getElementById("direct-support").checked,
+        document.getElementById("search-filter-data").checked,
+        document.getElementById("submit-reports").checked,
+        document.getElementById("manage-user-account").checked,
+        document.getElementById("climatic-consult").checked,
+        document.getElementById("view-data").checked,
+        document.getElementById("website-management").checked,
+        document.getElementById("manage-content").checked,
+        document.getElementById("manage-settings").checked,
+        document.getElementById("monitor-activity").checked,
+        document.getElementById("manage-security").checked];
+
     let table = document.getElementById("profile-table");
     let row = table.insertRow();
 
     profileIndex++;
 
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
-    row.insertCell().innerText = document.getElementById("");
+    row.insertCell().innerText = profileIndex.toString();
+    row.insertCell().innerText = document.getElementById("profile-name").value;
+
+    for (let i = 0; i < permissions.length; i++) {
+        if (permissions[i] === true) {
+            row.insertCell().innerText = "Yes"
+
+        } else {
+            row.insertCell().innerText = "No";
+        }
+    }
 
     alert("Profile added");
 }
