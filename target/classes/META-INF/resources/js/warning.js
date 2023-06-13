cc3627552ee3c813ef97ef03289461d9163a094a
Rodrigo <80919456+cw-rodrigo@users.noreply.github.com>
2023-06-12 21:10:44 -0300

    Updated to 130
    Removed target directory from version control

--- target/classes/META-INF/resources/js/warning.js
+++
@@ -1,9 +0,0 @@
-
-function disconnect(url) {
-    // just warns the user about the ongoing disconnection.
-    if (window.confirm("This action will disconnect you. Click 'Ok' to sign out or 'Cancel' to remain connected.")) {
-        location.href = url;
-    } else {
-        // the code won't do anything, the user will remain connected.
-    }
-}
