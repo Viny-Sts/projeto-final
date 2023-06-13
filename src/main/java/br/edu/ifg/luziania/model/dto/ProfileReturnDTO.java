f607363c1d101b0039edacf2d3e119274ac96923
rodrigo <limaosuco@gmail.com>
2023-06-12 22:01:06 -0300

    StormTrack - 156
    Changelog:
    - Updated ProfileReturnDTO constructor, to be able to receive profile entity as a parameter

--- src/main/java/br/edu/ifg/luziania/model/dto/ProfileReturnDTO.java
+++ src/main/java/br/edu/ifg/luziania/model/dto/ProfileReturnDTO.java
@@ -1,13 +1,25 @@
 package br.edu.ifg.luziania.model.dto;
 
+import br.edu.ifg.luziania.model.entity.Profiles;
+
+import java.util.List;
+
 public class ProfileReturnDTO {
     private Integer status;
 
     private String message;
 
+    private List<Profiles> profiles;
+
     public ProfileReturnDTO(Integer status, String message) {
         this.status = status;
         this.message = message;
+    }
+
+    public ProfileReturnDTO(Integer status, String message, List<Profiles> profiles) {
+        this.status = status;
+        this.message = message;
+        this.profiles = profiles;
     }
 
     public Integer getStatus() {
@@ -24,5 +36,13 @@ public class ProfileReturnDTO {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public List<Profiles> getProfiles() {
+        return profiles;
+    }
+
+    public void setProfiles(List<Profiles> profiles) {
+        this.profiles = profiles;
     }
 }
