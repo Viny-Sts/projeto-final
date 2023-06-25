2101ac82e4fdeeafc76207e6bdbf0384dda78342
rodrigo <limaosuco@gmail.com>
2023-06-25 13:15:20 -0300

    StormTrack - 163
    Changelog:
    - Added activity BO, DAO, DTO and ReturnDTO java classes

---
+++ src/main/java/br/edu/ifg/luziania/model/bo/ActivityBO.java
@@ -0,0 +1,37 @@
+package br.edu.ifg.luziania.model.bo;
+
+import br.edu.ifg.luziania.model.dao.ActivityDAO;
+import br.edu.ifg.luziania.model.dto.ActivityDTO;
+import br.edu.ifg.luziania.model.dto.ActivityReturnDTO;
+import br.edu.ifg.luziania.model.entity.Activity;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+
+@Dependent
+public class ActivityBO {
+    @Inject
+    ActivityDAO activityDAO;
+
+    @Transactional
+    public ActivityReturnDTO save(ActivityDTO activityDTO) {
+        try {
+            Activity activity = new Activity();
+            activityDAO.save(activity);
+
+            return new ActivityReturnDTO(200, "Activity successfully recorded!");
+
+        } catch (Exception exception) {
+            return new ActivityReturnDTO(500, "An error has occurred when recording activity");
+        }
+    }
+
+    public ActivityReturnDTO list() {
+        if (activityDAO.getAllActivity() == null)
+            return new ActivityReturnDTO(500, "There's no activity recorded");
+
+        return new ActivityReturnDTO(200, "Activity successfully retrieved!", activityDAO.getAllActivity());
+    }
+}
---
+++ src/main/java/br/edu/ifg/luziania/model/dto/ActivityDTO.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.luziania.model.dto;
+
+public class ActivityDTO {
+    private String activityLog;
+
+    public String getActivityLog() {
+        return activityLog;
+    }
+
+    public void setActivityLog(String activityLog) {
+        this.activityLog = activityLog;
+    }
+}
