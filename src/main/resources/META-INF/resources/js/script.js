e3869ef33e708e3c3cd571fe10b09af21d4eebd0
rodrigo <limaosuco@gmail.com>
2023-07-19 10:57:23 -0300

    StormTrack - 206 Changelog - Fixed weather panel duplicating bug - Added front-end to second use case - Refactored script.js

--- src/main/resources/META-INF/resources/js/script.js
+++ src/main/resources/META-INF/resources/js/script.js
@@ -11,31 +11,12 @@ function getOpenWeatherData() {
                 throw new Error("An error has occurred" + response.status);
 
         }).then(json => {
-            let weatherPanel = document.getElementById("weatherPanel");
+            clearWeatherPanel();
 
-            let cityInfo = document.createElement("p");
-            let weatherDescription = document.createElement("p");
-            let weatherTemp = document.createElement("p");
-            let weatherFeelsLike = document.createElement("p");
-            let weatherMax = document.createElement("p");
-            let weatherMin = document.createElement("p");
-            let weatherHumidity = document.createElement("p");
-
-            cityInfo.innerHTML = json.name + " - " + json.sys.country;
-            weatherDescription.innerHTML = json.weather[0].description;
-            weatherTemp.innerHTML = "Temperature: " + json.main.temp + " C°";
-            weatherFeelsLike.innerHTML = "Feels Like: " + json.main.feels_like + " C°";
-            weatherMin.innerHTML = "Min: " + json.main.temp_min + " C°";
-            weatherMax.innerHTML = "Max: " + json.main.temp_max + " C°";
-            weatherHumidity.innerHTML = "Humidity: " + json.main.humidity;
-
-            weatherPanel.appendChild(cityInfo);
-            weatherPanel.appendChild(weatherDescription);
-            weatherPanel.appendChild(weatherTemp);
-            weatherPanel.appendChild(weatherFeelsLike);
-            weatherPanel.appendChild(weatherMax);
-            weatherPanel.appendChild(weatherMin);
-            weatherPanel.appendChild(weatherHumidity);
+            updateWeatherPanel(json.name + " " + json.sys.country, capitalizeFirstLetter(json.weather[0].description),
+                json.main.temp + " C°" + " - Feels Like " + json.main.feels_like + " C°",
+                "Min " + json.main.temp_min + " C°" + " - " + "Max " + json.main.temp_max + " C°",
+                "Humidity " + json.main.humidity)
         }
     );
 }
@@ -43,7 +24,7 @@ function getOpenWeatherData() {
 function getFutureOpenWeatherData() {
     let city = document.getElementById("city").value
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=" + apiKey)
         .then((response) => {
             if (response.status === 200)
                 return response.json();
@@ -51,26 +32,64 @@ function getFutureOpenWeatherData() {
                 throw new Error("An error has occurred" + response.status);
 
         }).then(json => {
-            let data1 = "";
+            clearWeatherPanel();
 
-            const forecasts = json.list.filter((forecast, index) => index % 8 === 0);
-
-            let weatherPanel = document.getElementById("weatherPanel");
-            let info = document.createElement("p");
-
-            forecasts.forEach(forecast => {
-                const dataHora = forecast.dt_txt;
-                const temperatura = forecast.main.temp;
-                const descricao = forecast.weather[0].description;
-
-                console.log(`Data e Hora: ${dataHora}, Temperatura: ${temperatura} K, Descrição: ${descricao}`);
-                data1 += `Data e Hora: ${dataHora}, Temperatura: ${temperatura} K, Descrição: ${descricao} \n`
-            });
-
-            info.innerHTML = data1;
-
-            weatherPanel.appendChild(info);
+            for (let i = 0; i < json.list.length; i++) {
+                updateWeatherPanel(json.city.name + " " + json.city.country + " - " + json.list[i].dt_txt, capitalizeFirstLetter(json.list[i].weather[0].description),
+                    json.list[i].main.temp + " C°" + " - Feels Like " + json.list[i].main.feels_like + " C°",
+                    "Min " + json.list[i].main.temp_min + " C°" + " - Max " + json.list[i].main.temp_max + " C°",
+                    "Humidity " + json.list[i].main.humidity);
+            }
     });
+}
+
+function updateWeatherPanel(city, description, temp, minMax, humidity) {
+    let weatherPanel = document.getElementById("weatherPanel");
+
+    let cityInfo = document.createElement("p");
+    let weatherDescription = document.createElement("p");
+    let weatherTemp = document.createElement("p");
+    let weatherMinMax = document.createElement("p");
+    let weatherHumidity = document.createElement("p");
+
+    let breakLine = document.createElement("br");
+
+    cityInfo.id = "city-info";
+    weatherDescription.id = "weather-description";
+    weatherTemp.id = "weather-temp";
+    weatherMinMax.id = "weather-min-max";
+    weatherHumidity.id = "weather-humidity";
+
+    breakLine.id = "break-line";
+
+    cityInfo.innerHTML = city;
+    weatherDescription.innerHTML = description;
+    weatherTemp.innerHTML = temp;
+    weatherMinMax.innerHTML = minMax;
+    weatherHumidity.innerHTML = humidity;
+
+    weatherPanel.appendChild(cityInfo);
+    weatherPanel.appendChild(weatherDescription);
+    weatherPanel.appendChild(weatherTemp);
+    weatherPanel.appendChild(weatherMinMax);
+    weatherPanel.appendChild(weatherHumidity);
+
+    weatherPanel.appendChild(breakLine);
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function clearWeatherPanel() {
+    while (document.getElementById("city-info")) {
+        document.getElementById("city-info").remove();
+        document.getElementById("weather-description").remove();
+        document.getElementById("weather-temp").remove();
+        document.getElementById("weather-min-max").remove();
+        document.getElementById("weather-humidity").remove();
+        document.getElementById("break-line").remove();
+    }
 }
 
 function clearFields() {
